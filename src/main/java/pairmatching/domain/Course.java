3fdb68d88d951b2ecac1eae8b11c505cf2d4fbb0
Be-student <presentsong@naver.com>
2022-12-09 01:42:05 +0900

    Terminal feat: 코스를 생성하는 정적 팩토리 메서드를 추가한다

--- src/main/java/pairmatching/domain/Course.java
+++ src/main/java/pairmatching/domain/Course.java
@@ -1,14 +1,21 @@
 package pairmatching.domain;
 
+import java.util.Arrays;
+
 public enum Course {
     BACKEND("백엔드"),
     FRONTEND("프론트엔드");
 
-    private String name;
+    private final String name;
 
     Course(String name) {
         this.name = name;
     }
 
-    // 추가 기능 구현
+    public static Course getFromName(String name) {
+        return Arrays.stream(Course.values())
+                .filter(it -> it.name.equals(name))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException("코스 이름이 잘못되었습니다"));
+    }
 }
