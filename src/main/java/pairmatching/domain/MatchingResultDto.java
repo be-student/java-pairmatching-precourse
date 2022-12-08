b69ac5fb9d07c03d2101e84c7455b2fac9f879f9
Be-student <presentsong@naver.com>
2022-12-09 02:46:11 +0900

    feat: 결과 계산 조회 기능 추가

--- src/main/java/pairmatching/domain/MatchingResultDto.java
+++ src/main/java/pairmatching/domain/MatchingResultDto.java
@@ -1,5 +1,16 @@
 package pairmatching.domain;
 
+import java.util.List;
+
 public class MatchingResultDto {
 
+    private final List<String> result;
+
+    public MatchingResultDto(List<String> result) {
+        this.result = result;
+    }
+
+    public List<String> getResult() {
+        return result;
+    }
 }
