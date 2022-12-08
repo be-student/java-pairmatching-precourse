b4e210e035212f3b272571df1ea837c2c12fb246
Be-student <presentsong@naver.com>
2022-12-09 02:35:22 +0900

    feat: map에 키로 쓰기 위해서 equals,hashcode 오버라이딩을 한다

--- src/main/java/pairmatching/domain/Search.java
+++ src/main/java/pairmatching/domain/Search.java
@@ -1,14 +1,15 @@
 package pairmatching.domain;
 
+import java.util.Objects;
 import java.util.regex.Pattern;
 
 public class Search {
 
-    private static final Pattern SEARCH = Pattern.compile(
+    private static final Pattern FORMAT = Pattern.compile(
             "^(백엔드|프론트엔드), 레벨[1-5], (자동차경주|로또|숫자야구게임|장바구니|결제|지하철노선도|성능개선|배포)$");
-    private final Course course;
     private final Level level;
     private final Mission mission;
+    private final Course course;
 
     public Search(String input) {
         validate(input);
@@ -19,8 +20,27 @@ public class Search {
     }
 
     private void validate(String input) {
-        if (SEARCH.matcher(input).matches()) {
+        if (FORMAT.matcher(input).matches()) {
             throw new IllegalArgumentException("형식을 다시 확인해 주세요");
         }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Search)) {
+            return false;
+        }
+        if (course != ((Search) o).course) {
+            return false;
+        }
+        if (level != ((Search) o).level) {
+            return false;
+        }
+        return mission == ((Search) o).mission;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(level, mission, course);
     }
 }
