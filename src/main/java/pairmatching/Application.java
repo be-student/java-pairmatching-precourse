3c71bc507d578e30c419f70bc9f3adf9633bc2d8
Be-student <presentsong@naver.com>
2022-12-09 16:33:52 +0900

    애플리케이션

--- src/main/java/pairmatching/Application.java
+++ src/main/java/pairmatching/Application.java
@@ -1,7 +1,24 @@
 package pairmatching;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+import pairmatching.domain.Course;
+import pairmatching.domain.Crew;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO 구현 진행
+        try {
+            List<Crew> crews = new ArrayList<>();
+            crews.add(new Crew(Course.BACKEND, "test"));
+            crews.add(new Crew(Course.BACKEND, "hello"));
+            List<Crew> temp = Randoms.shuffle(crews);
+            System.out.println(temp.get(0).getClass());
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
     }
+
 }
