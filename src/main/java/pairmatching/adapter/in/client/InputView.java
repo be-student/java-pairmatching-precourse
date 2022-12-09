8830b06831fba9735b3689c5185bab90adde850d
Be-student <presentsong@naver.com>
2022-12-09 16:04:55 +0900

    feat: strategy 로 return하도록 수정

--- src/main/java/pairmatching/adapter/in/client/InputView.java
+++ src/main/java/pairmatching/adapter/in/client/InputView.java
@@ -27,13 +27,13 @@ public class InputView {
         System.out.println("ex) 백엔드, 레벨1, 자동차경주");
     }
 
-    public static Function askFunction() {
+    public static PairMatchingStrategy askPairMatchingStrategy() {
         System.out.println("기능을 선택하세요.");
         System.out.println("1. 페어 매칭");
         System.out.println("2. 페어 조회");
         System.out.println("3. 페어 초기화");
         System.out.println("Q. 종료");
-        return Function.getFromFunctionInput(Console.readLine());
+        return PairMatchingStrategy.getFromCommand(Console.readLine());
     }
 
     public static ReMatching askReMatching() {
