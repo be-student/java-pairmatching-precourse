1a301cc5d13228776f911870d706cf04b957eea2
Be-student <presentsong@naver.com>
2022-12-09 13:50:43 +0900

    fix: 안 쓰이는 level 제거

--- src/main/java/pairmatching/domain/Search.java
+++ src/main/java/pairmatching/domain/Search.java
@@ -7,7 +7,7 @@ public class Search {
 
     private static final Pattern FORMAT = Pattern.compile(
             "^(백엔드|프론트엔드), 레벨[1-5], (자동차경주|로또|숫자야구게임|장바구니|결제|지하철노선도|성능개선|배포)$");
-    private final Level level;
+
     private final Mission mission;
     private final Course course;
 
@@ -33,14 +33,11 @@ public class Search {
         if (course != ((Search) o).course) {
             return false;
         }
-        if (level != ((Search) o).level) {
-            return false;
-        }
         return mission == ((Search) o).mission;
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(level, mission, course);
+        return Objects.hash(mission, course);
     }
 }
